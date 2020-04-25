3525aa028be9dc8da9983002addafc8f4b9922f4
David Yu <davidpyu@gmail.com>
2020-04-24 23:02:56 -0500

    browser opens, click county, screenshot

--- app.js
+++ app.js
@@ -1,6 +1,28 @@
 const puppeteer = require('puppeteer');
+const shouldExit = 0;
+
+const delay = (t, val) => {
+  return new Promise(function(resolve) {
+      setTimeout(function() {
+          resolve(val);
+      }, t);
+  });
+}
 
 const getCovid19 = async () => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({headless: false});
+  const page = await browser.newPage();
+  await page.setViewport({ width: 1920, height: 1080});
+
+  await page.goto('https://www.dph.illinois.gov/covid19/covid19-statistics');
+  // await page.evaluate('document.querySelector(\'.pagination > li:first-child > a\').click();');
   
+  await page.click('.pagination > li:first-child > a');
+  await delay(2000);
+
+  await page.screenshot({path: 'test.png'});
+
+  await browser.close();
 };
+
+getCovid19();
