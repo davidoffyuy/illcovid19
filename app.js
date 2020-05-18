92e67eba58673cdae06155e34b1bc8ebc059d908
David Yu <davidpyu@gmail.com>
2020-05-18 00:59:46 -0500

    Get number of dates for county

--- app.js
+++ app.js
@@ -14,20 +14,27 @@ const getCovid19 = async () => {
 
   await page.goto('https://www.dph.illinois.gov/covid19/covid19-statistics');
   
-  // Click on 'By County' to show statistics and sort by each county. Delay is used to allow SPA function to complete.
+  //Click on 'By County' to show statistics and sort by each county. Delay is used to allow SPA function to complete.
   await page.click('.pagination > li:first-child > a');
   await delay(2000);
 
-  // Input County into input field
+  //Input County into input field
   await page.focus('#input-filter');
   page.keyboard.type(county);
   await delay (1000);
 
-  const dateListElement = await page.$('#paginMap');
-  const testValue = await page.evaluate(el => el.querySelector('li:nth-last-child(2) > a').innerHTML, dateListElement);
+  //Get the number of dates displayed. Last element is a arrow and not a date.
+  const dateListElements = await page.$$('#paginMap > li');
+  const dateLength = dateListElements.length - 1;
+
+  // const testValue = await page.evaluate(el => el.querySelector('li:nth-last-child(2) > a').innerHTML, dateListElement);
+  // const listElements = await dateListElement.$$('li');
+  // console.log('Length: ' + listElements.length);
+  // const testValue = await page.evaluate(el => el.querySelector('a').innerHTML, listElements[2]);
+  // console.log('testValue: ' + testValue);
+
   // const testValue = await page.evaluate(el => {
   //   'document.querySelector(\'li:last-child(2) > a\').innerHTML;');
-  console.log("testValue: " + testValue);
 
   // Test screenshot to show page has loaded
   // await page.screenshot({path: 'test.png'});
